4900ecb05b233ec9f0f2c70f78684ae85202b364
playfulCloud <godlygener@gmail.com>
2024-03-31 19:26:49 +0200

    board

---
+++ cmake-build-debug/CMakeFiles/SO2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SO2.dir/main.cpp.o"
+  "CMakeFiles/SO2.dir/main.cpp.o.d"
+  "SO2"
+  "SO2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SO2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
